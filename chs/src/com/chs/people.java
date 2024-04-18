de9fbf7a34a0df9e74c0462081627799b759c204
JSW <JSW@DESKTOP-NET8S48>
2024-04-18 17:42:03 +0900

    jsw_수정함

--- chs/src/com/chs/people.java
+++ chs/src/com/chs/people.java
@@ -2,8 +2,22 @@ package com.chs;
 
 public class people {
 	private String name;
-public people() {
-	// TODO Auto-generated constructor stub
-}
+	private int age;
+	public people() {
+		// TODO Auto-generated constructor stub
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
 
 }
